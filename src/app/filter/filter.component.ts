b357ad5ac5dc30fc2115a57b7a85d51be9c6418f
ariqyandri <ariq140199@gmail.com>
2022-10-04 18:09:26 +0200

    add notes

--- src/app/filter/filter.component.ts
+++ src/app/filter/filter.component.ts
@@ -4,38 +4,30 @@ import { ActivatedRoute, Router } from '@angular/router';
 @Component({
   selector: 'package-filter',
   templateUrl: './filter.component.html',
-  styleUrls: ['./filter.component.scss']
+  styleUrls: ['./filter.component.scss'],
 })
 export class FilterComponent implements OnInit {
-
   constructor(private route: ActivatedRoute, private router: Router) {}
-  options = [
-    { id: 1, name: 'Option 1' },
-    { id: 2, name: 'Option 2' },
-    { id: 3, name: 'Option 3' },
-  ];
 
+  // Idealy should be another service for each
   airports = [
     { id: 2, name: 'Schipol' },
     { id: 3, name: 'Heathrow' },
   ];
+
   countries = [
     { id: 7, name: 'Italy' },
     { id: 9, name: 'Spain' },
-    { id: 11, name: 'Belgion' },
+    { id: 11, name: 'Belgium' },
   ];
-  // submit(f: any) {
-  //   console.log(f);
-  // }
 
   submit(f: any) {
-    console.log(f)
     this.router.navigate(['/packages'], {
       queryParams: { depart: f.depart, country: f.country },
     });
   }
+
   ngOnInit(): void {
-
+    // Here subscribe to the query params to give initial value to the form , if there is
   }
-
 }
