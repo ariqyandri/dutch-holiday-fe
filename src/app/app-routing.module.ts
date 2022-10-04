2322dd704a6bcd763e72351d09cfb2113fbd573f
ariqyandri <ariq140199@gmail.com>
2022-10-04 11:54:32 +0200

    update navbar

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
+import { PackagesComponent } from './packages/packages.component';
 
 const routes: Routes = [
   {
     path: '',
     component: HomeComponent,
+  },
+  {
+    path: 'packages',
+    component: PackagesComponent,
   },
 ];
 
